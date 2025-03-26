ddd88a5cba6e59165c4bd20de8c665845069d3d1
Sumesh Sivan <sumesh@credot.in>
2025-03-26 13:02:48 +0530

    Players and team search issue fix

--- src/services/teamService.js
+++ src/services/teamService.js
@@ -48,11 +48,6 @@ class TeamService {
       query.status = status;
     }
 
-    if (search) {
-      query.team = query.team || {};
-      query.team.name = { $regex: search, $options: 'i' };
-    }
-
     let teams = await TournamentTeams.find(query)
       .populate({
         path: 'team',
@@ -62,6 +57,9 @@ class TeamService {
       })
       .sort({ createdAt: -1 });
 
+    if (search) {
+      teams = teams.filter(team => team.team.name.toLowerCase().includes(search.toLowerCase()));
+    }
     return teams;
   }
 
