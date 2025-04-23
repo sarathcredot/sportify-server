6ad31185ce85e038995c9085540d9f69d7aa8f72
Sumesh Sivan <sumesh@credot.in>
2025-04-23 16:35:27 +0530

    Search tournament List

--- src/routes/admin/tournament.js
+++ src/routes/admin/tournament.js
@@ -15,6 +15,20 @@ const tournamentController = new TournamentController();
  *     tags: [Admin]
  *     security:
  *       - bearerAuth: []
+ *     parameters:
+ *       - name: search
+ *         in: query
+ *         description: Search query
+ *         schema:
+ *           type: string
+ *       - name: page
+ *         in: query
+ *         description: Page number
+ *         schema:
+ *           type: number
+ *       - name: limit
+ *         in: query
+ *         description: Limit
  *     responses:
  *       200:
  *         description: List of tournaments
