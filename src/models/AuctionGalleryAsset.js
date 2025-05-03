1a82b319f379a93e5c99118b1a63678e87a05216
Sumesh Sivan <sumesh@credot.in>
2025-05-03 23:19:26 +0530

    Auction gallery asset refactoring

--- src/models/AuctionGalleryAsset.js
+++ src/models/AuctionGalleryAsset.js
@@ -4,6 +4,7 @@ const auctionGalleryAssetSchema = new mongoose.Schema({
   assetUrl: { type: String, required: true },
   type: { type: String, enum: ['image', 'video'], required: true },
   auction: { type: mongoose.Schema.Types.ObjectId, ref: 'Auction', required: true },
+  isActive: { type: Boolean, default: true },
 }, {
   timestamps: true
 });
