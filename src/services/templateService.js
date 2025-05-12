1fc180a2ea5dd13318f2daba61974605d0c41026
Sumesh Sivan <sumesh@credot.in>
2025-05-12 16:14:18 +0530

    Full page render

--- src/services/templateService.js
+++ src/services/templateService.js
@@ -89,7 +89,7 @@ class TemplateService {
       // Generate the PNG
       const buffer = await page.screenshot({
         type: 'png',
-        fullPage: false,
+        fullPage: true,
         encoding: 'binary'
       });
 
