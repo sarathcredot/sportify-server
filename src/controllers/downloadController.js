cd62412346b1efe15c6eb2964cf6f6c4ce1333f7
sarathcredot <sarath@credot.in>
2025-06-05 15:10:37 +0530

    team details export to pdf added

--- src/controllers/downloadController.js
+++ src/controllers/downloadController.js
@@ -30,5 +30,28 @@ module.exports = {
 
 
         }
+    },
+
+
+    downloadTournamentTeam:async(req,res)=>{
+        try {
+
+            const { tournamentId } = req.params;
+
+            console.log("downloadTournamentTeam called", tournamentId);
+
+            const result = await downloadService.downloadTournamentTeam(tournamentId);
+
+            res.setHeader("Content-Type", "application/pdf");
+            res.setHeader("Content-Disposition", "attachment; filename=teams.pdf");
+            res.json(ResponseHandler.success("tournament teams document", result));
+
+        } catch (error) {
+
+            console.log("downloadTournamentTeam error", error);
+
+            BaseController.handleError(res, error);
+
+        }
     }
 }
