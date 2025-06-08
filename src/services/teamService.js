e57c8da121af4a375f5d14d74f8cb5b7b068c984
Sumesh Sivan <sumesh@credot.in>
2025-06-08 16:03:12 +0530

    is registered boolean on tournament details api for team manager

--- src/services/teamService.js
+++ src/services/teamService.js
@@ -21,29 +21,28 @@ class TeamService {
       throw new ValidationError("Tournament registration is closed");
     }
 
-
-    const managerTeam = await Team.find({ manager: teamData.manager });
-    console.log("managerteam data", managerTeam)
-
-    if (managerTeam) {
-      for (let elm of managerTeam) {
-
-        const exitingManager = await TournamentTeams.findOne({ tournament: tournamentId, team: elm?._id })
-
-        if (exitingManager) {
-
-          throw new ValidationError("You have already registered a team for this tournament");
-
-        }
-      }
+    const isAlreadyRegistered = await TournamentTeams.findOne({ tournament: tournamentId, teamManager: teamData.manager });
+    if (isAlreadyRegistered) {
+      throw new ValidationError("You have already registered a team for this tournament");
     }
 
+    // const managerTeam = await Team.find({ manager: teamData.manager });
+    // console.log("managerteam data", managerTeam)
+
+    // if (managerTeam) {
+    //   for (let elm of managerTeam) {
+    //     const exitingManager = await TournamentTeams.findOne({ tournament: tournamentId, team: elm?._id })
+    //     if (exitingManager) {
+    //       throw new ValidationError("You have already registered a team for this tournament");
+    //     }
+    //   }
+    // }
 
     const maxTeamAllowed = tournament?.settings?.maxTeamAllowed;
     const totalTeams = await TournamentTeams.find({ tournament: tournamentId, status: TEAM_STATUS.APPROVED })
 
     if (totalTeams.length === maxTeamAllowed) {
-      throw new NotFoundError("Maximum allowed teams reached");
+      throw new ValidationError("Maximum allowed teams reached");
     }
 
     const team = new Team({
@@ -64,6 +63,7 @@ class TeamService {
       name: teamData.name,
       phoneNumber: teamData.phoneNumber,
       email: teamData.email,
+      teamManager: teamData.manager
     });
     // await sendEmail(teamData.email, "Team Registration Confirmation", `You have successfully registered your team: ${teamData.name}. Your Team ID is ${teamId}.`);
 
@@ -174,7 +174,7 @@ class TeamService {
     //   });
     // }
 
-    return await this.createTeam(teamData, teamData.tournamentId, TEAM_STATUS.PENDING);
+    return await this.createTeam(teamData.tournamentId, TEAM_STATUS.PENDING);
   }
 
   async generateTeamId(tournament) {
