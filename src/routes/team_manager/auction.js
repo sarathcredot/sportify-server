319ccbbcd74791fff543e1c67c2bae6ee0524b2c
Sumesh Sivan <sumesh@credot.in>
2025-06-25 00:52:59 +0530

    Gallery api for team manager

--- src/routes/team_manager/auction.js
+++ src/routes/team_manager/auction.js
@@ -27,6 +27,31 @@ const auctionController = new AuctionController();
  */
 router.get('/', auctionController.getTeamManagerAuctions);
 
+/**
+ * @swagger
+ * /organiser/auction/{auctionId}/gallery:
+ *   get:
+ *     summary: Get gallery
+ *     tags: [Organiser]
+ *     security:
+ *       - bearerAuth: []
+ *     parameters:
+ *       - name: auctionId
+ *         in: path
+ *         required: true
+ *         description: The ID of the auction
+ *     responses:
+ *       200:
+ *         content:
+ *           application/json:
+ *             schema:
+ *               allOf:
+ *                 - $ref: '#/components/schemas/ApiResponse'
+ *                 - type: object
+ *         description: Gallery retrieved
+ */
+router.get("/:auctionId/gallery", auctionController.getGallery);
+
 router.post('/:auctionId/place-concealed-bid', auctionController.placeConcealedBid);
 
 module.exports = router;
