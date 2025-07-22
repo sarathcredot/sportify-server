f71672bae7644fcf5652571c8f4acf3350fa5013
Sumesh Sivan <sumesh@credot.in>
2025-07-23 01:46:56 +0530

    Change age to dob in Teammanager player model

--- src/models/TeamManagerPlayer.js
+++ src/models/TeamManagerPlayer.js
@@ -5,7 +5,7 @@ const teamManagerPlayerSchema = new mongoose.Schema(
     fullName: { type: String, required: true, trim: true },
     position: { type: String, required: true, trim: true },
     photoUrl: { type: String, required: true },
-    age: { type: Number, required: true },
+    dateOfBirth: { type: Date, required: true },
     notes: { type: String },
     teamManager: { 
       type: mongoose.Schema.Types.ObjectId,
